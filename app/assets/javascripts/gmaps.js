25b41c492dce6573777a20b378cd5a15f22e0eef
lauraconwill <lauraconwill@gmail.com>
2014-03-23 15:47:24 -0400

    dropdowns on map now correspond to correct locations

--- app/assets/javascripts/gmaps.js
+++ app/assets/javascripts/gmaps.js
@@ -1,16 +1,25 @@
 var directionsDisplay;
 var directionsService = new google.maps.DirectionsService();
 
+
 function initialize() {
   directionsDisplay = new google.maps.DirectionsRenderer();
   var mapOptions = {
     zoom: 7,
     center: new google.maps.LatLng(40.757395, -73.989977)
   };
+
   var map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
   directionsDisplay.setMap(map);
   directionsDisplay.setPanel(document.getElementById('directions-panel'));
+
+  var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+  var marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title:"Hello World!"
+  });
 
   var control = document.getElementById('control');
   control.style.display = 'block';
